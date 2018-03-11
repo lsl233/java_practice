0cb4babb4de1583ebb5c45f9f33505ad129dbf55
lsl233 <ahands233@gmail.com>
2018-03-11 10:35:15 +0800

    充值

---
+++ src/Recharge.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Recharge {
+    public static void main(String[] args) {
+        // 浮点数不能直接比较
+        double a = 1.0;
+        double b = 0.5 + 0.5;
+        System.out.println(b);
+        System.out.println(Math.abs(a - b) < 1e-6);
+        Scanner in = new Scanner(System.in);
+        System.out.print("请投币：");
+        int amount = in.nextInt();
+        System.out.println("*************");
+        System.out.println("***票价10元***");
+        System.out.println("*************");
+
+        System.out.println("找零：" + (amount - 10));
+    }
+}
